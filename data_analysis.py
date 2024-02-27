1f056f4494aad5c8da60a5a34cd8aec48b001479
Jonathan Yoni Hirshowitz <JHirshowitz@visuallease.com>
2024-02-27 10:58:36 -0500

    refactored function

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,24 @@
 import json
 from log import Log
+# logs = [
+#             {
+#                 "Subaru":{
+#                     "sum": 100,
+#                     "count": 1
+#                 }
+#             }
+#         ]
+logs=[]
+#def avg_speed_by_model(fileName):
+    #with open(fileName, 'r') as logger:
+        # data = json.loads(logger)
+        
+        # for log in data:
+        #     print(type(log))
+        #     model = log.models
+        #     #new_log = Log(log['models'], log.fromGear, log.toGear, log.speed)
+        #     #logs.append(new_log)
+        #     print(type(model))
 
-def avg_speed_by_model(fileName):
-    with open(fileName) as logger:
-        data = json.load(logger)
 
-        for log in data:
-            print(type(log))
-            if log.from_gear == 3 and log.to_gear == 4:
-                print(log)
+        
--- log.py
+++ log.py
@@ -1,6 +1,10 @@
         
+from datetime import date
 import json
 import os
+
+change_in_gear_file_name = "change_in_gear.json"
+gear_error_file_name = "log_error.json"
 
 class Log:
     def __init__(self, model, vin, from_gear, to_gear, speed, time_stamp):
@@ -12,20 +16,27 @@ class Log:
         self.time_stamp = time_stamp
 
     def describe(self):
-        print("log model: {}, vin: {}, fromGear: {}, toGear: {}, speed:{}, timeStamp:{}".format(self.model, self.vin, self.fromGear, self.toGear, self.gear, self.speed, self.timeStamp))
+        print("log model: {}, vin: {}, fromGear: {}, toGear: {}, speed:{}, timeStamp:{}"
+              .format(self.model, self.vin, self.fromGear, self.toGear, self.gear, self.speed, self.timeStamp))
+
+def empty_logger(fileName):
+    if os.path.exists(fileName):
+        data = []
+        with open(fileName, "w") as logs:
+            json.dump(data, logs)
 
 def log(fileName, loggingInfo):
-    json_object = json.dumps(loggingInfo, default=vars, indent=4)
-
+    loggingInfoJson = json.dumps(loggingInfo, default=vars, indent=4)
+    
     if os.path.exists(fileName):
         if os.stat(fileName).st_size == 0:
             data = []
         else:
             with open(fileName) as logs:
                 data = json.load(logs)
-        data.append(json_object)
+        data.append(loggingInfoJson)
     else:
-        data = [json_object]
+        data = [loggingInfoJson]
                 
     with open(fileName, "w") as logs:
         json.dump(data, logs)
@@ -36,3 +47,26 @@ def print_out_log(fileName):
     
     for log in data:
         print(log)
+
+def populate_logs(input_cars, car_inventories, vin_models):
+    empty_logger(change_in_gear_file_name)
+    empty_logger(gear_error_file_name)
+
+    strftime = "%d/%m/%Y %H:%M:%S"
+
+    prevGear = input_cars[0].gear
+    for input_car in input_cars:
+        vin = input_car.vin
+        model = [vin_model.model for vin_model in vin_models if vin_model.vin == vin][0]
+        
+        new_log = Log(model, vin, prevGear, input_car.gear, input_car.speed, date.today().strftime(strftime))
+
+        if input_car.gear != prevGear:
+            log(change_in_gear_file_name, new_log)
+            prevGear = input_car.gear
+
+        gearAndMaxSpeedForModel = [car_inventory.gears_max_speeds for car_inventory in car_inventories if car_inventory.model == model]
+        maxSpeed = [gearAndMaxSpeed['max_speed'] for gearAndMaxSpeed in gearAndMaxSpeedForModel[0] if gearAndMaxSpeed['gear'] == input_car.gear][0]
+        
+        if(input_car.speed > maxSpeed):
+            log(gear_error_file_name, new_log)
