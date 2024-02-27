b28e5aa8b28a2570a4bd9af85f7ca1de37212220
Jonathan Yoni Hirshowitz <JHirshowitz@visuallease.com>
2024-02-27 13:07:07 -0500

    fixed

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 
 import unittest
-from data_analysis import avg_speed_by_model
+#from data_analysis import avg_speed_by_model
 from main import main
 
 
@@ -11,13 +11,12 @@ class MainTest(unittest.TestCase):
 
     def test_project_runs(self):
         #arrange
-        change_in_gear_file_name = "change_in_gear.json"
+        change_in_gear_file_name = "log_change_in_gear.json"
         gear_error_file_name = "log_error.json"
         model = "Subaru"
-        #main(change_in_gear_file_name, gear_error_file_name)
 
         #act
-        avg_speed_by_model(change_in_gear_file_name, model)
+        main(change_in_gear_file_name, gear_error_file_name)
 
         #assert
         self.assertTrue(True)
