96345c992c20a07f914be1885cc9883089500336
Yoni Hirshowitz <jonhirsh39@gmail.com>
2024-02-28 11:40:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from load_data import load_car_inventory, load_input_car, load_vin_model
 from log import populate_logs
 def main(change_in_gear_file_name, gear_error_file_name):
-    print(main)
     input_cars = load_input_car('input_cars.json')
     car_inventories = load_car_inventory('car_inventory.json')
     vin_models = load_vin_model('vin_model.json')
